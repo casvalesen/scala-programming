0e6053f82269404604e2caec1b5cbce56128d66b
Christian Aalby Svalesen <christianaalbysvalesen@gmail.com>
2022-10-11 13:24:37 +0200

    added first scala example from coursera EPFL course, started language basics tour

---
+++ basics/language_basics.scala
@@ -0,0 +1,13 @@
+// Basic scala tour 
+// https://docs.scala-lang.org/tour/basics.html
+
+// Simple print statements
+println(1+1)
+println("Hello, world!")
+
+// Naming results of expressions with val keyword
+val x = 1 + 1 
+print(x)
+
+// The type of a value can be omitted and inferred, or explicitly stated
+val x: Int = 1 + 1 
